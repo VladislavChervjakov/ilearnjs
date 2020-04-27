02a8e2b88f37c2008792e40f55a5b1f6a2e71ffd
Vlad <vlchervjakov@gmail.com>
2020-04-27 17:06:59 +0300

    prototypes chap2

--- js/main.js
+++ js/main.js
@@ -887,5 +887,29 @@ let pockets = {
     __proto__: bed
 }
 
-alert(pockets.pen);
+// alert(pockets.pen);
 
+// function f() {
+//     alert("Hello");
+// }
+
+// Function.prototype.defer = function(ms) {
+//     setTimeout(this, ms);
+// };
+
+// f.defer(1000);
+
+function f(a, b) {
+    alert(a + b);
+}
+
+Function.prototype.defer = function(ms) {
+    let f = this;
+    return function(...args) {
+        // setTimeout(function() {f.apply(this, args)}, ms);
+        setTimeout(() => f.apply(this, args), ms);
+
+    };
+};
+
+f.defer(1000)(1, 2);

5b2ba96cbfeea3e84d005b4ad4555eab7289993b
Vlad <vlchervjakov@gmail.com>
2020-04-28 00:57:15 +0300

    classes chap1

--- js/main.js
+++ js/main.js
@@ -914,23 +914,62 @@ Function.prototype.defer = function(ms) {
 
 //f.defer(1000)(1, 2);
 
-let dictionary = Object.create(null, {
-    toString: {
-        value() {
-            return Object.keys(this).join();
-        }
-    }
-});
+// let dictionary = Object.create(null, {
+//     toString: {
+//         value() {
+//             return Object.keys(this).join();
+//         }
+//     }
+// });
 
-dictionary.apple = "Apple";
-dictionary.__proto__ = "test";
+// dictionary.apple = "Apple";
+// dictionary.__proto__ = "test";
 
- Object.defineProperty(dictionary, "toString", {
-    enumerable: false
-  });
+//  Object.defineProperty(dictionary, "toString", {
+//     enumerable: false
+//   });
 
-// for(let key in dictionary) {
-//     alert(key);
-// }
+// // for(let key in dictionary) {
+// //     alert(key);
+// // }
 
-alert(dictionary);
+// alert(dictionary);
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+
+let clock = new Clock({template: 'h:m:s'});
+clock.start();

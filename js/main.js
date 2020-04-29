e2fb0cfa4b9fa8ec0105a0fcc1d5d50e67476946
Vlad <vlchervjakov@gmail.com>
2020-04-30 00:39:44 +0300

    generators

--- js/main.js
+++ js/main.js
@@ -1050,5 +1050,19 @@ async function demoGithubUser() {
     alert(`Full name: ${user.name}`);
     return user;
 }
- demoGithubUser();
+//  demoGithubUser();
 
+function* pseudoRandom(seed) {
+    let value = seed;
+    while(true) {
+        value =  value * 16807 % 2147483647;
+        yield value;
+    }
+}
+
+let generator = pseudoRandom(1);
+
+alert(generator.next().value);
+alert(generator.next().value);
+alert(generator.next().value);
+alert(generator.next().value);

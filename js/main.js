c25a8f68a5d2e1e60a005d3e812469bd789f50e0
Vlad <vlchervjakov@gmail.com>
2020-05-06 23:05:36 +0300

    dom part4

--- js/main.js
+++ js/main.js
@@ -1181,20 +1181,225 @@ array = new Proxy(array, {
 //     alert(title + ": " + count);
 // }
 
-let data = document.querySelector('[data-widget-name]').dataset.widgetName;
+// let data = document.querySelector('[data-widget-name]').dataset.widgetName;
 
 // alert(data);
 
-let links = document.querySelectorAll("li > a");
+// let links = document.querySelectorAll("li > a");
 
-for(link of links) {
-    let href = link.getAttribute('href');
+// for(link of links) {
+//     let href = link.getAttribute('href');
 
-    if(!href) continue;
+//     if(!href) continue;
 
-    if(!href.includes('://')) continue;
+//     if(!href.includes('://')) continue;
 
-    if(href.startsWith('http://internal.com')) continue;
+//     if(href.startsWith('http://internal.com')) continue;
 
-    link.style.color = 'orange';
-}
+//     link.style.color = 'orange';
+// }
+
+// let div = document.createElement('div');
+// div.className = 'alert';
+// div.innerHTML = '<strong>Всем привет!</strong> Вы прочитали важное сообщение.';
+
+// document.body.append(div);
+
+// setTimeout(() => div.remove(), 1000);
+
+// let div2 = div.cloneNode(true);
+
+// div2.querySelector('strong').innerHTML = "Всем пока!";
+
+// div.after(div2);
+
+// function clear(elem) {
+//     while(elem.firstChild) {
+//         elem.firstChild.remove();
+//     }
+// }
+
+// or
+
+// function clear(elem) {
+//     elem.innerHTML = '';
+// }
+
+
+// elem = document.querySelector('ol');
+
+// clear(elem);
+
+// let ul = document.querySelector('.user-list');
+
+// while(true) {
+//     let liContent = prompt("Input text: ", null);
+
+//     if(liContent == null || liContent == '') break;
+
+//     let li = document.createElement('li');
+//     li.textContent = liContent;
+//     alert(li.textContent);
+
+//     ul.append(li);
+// }
+
+// let data = {
+//     "Рыбы": {
+//       "форель": {},
+//       "лосось": {}
+//     },
+  
+//     "Деревья": {
+//       "Огромные": {
+//         "секвойя": {},
+//         "дуб": {}
+//       },
+//       "Цветковые": {
+//         "яблоня": {},
+//         "магнолия": {}
+//       }
+//     }
+//   };
+
+//   function createTree(container, data) {
+//         container.append(createTreeObject(data));
+//   }
+
+//   function createTreeObject(obj) {
+//       if(!Object.keys(obj).length) return;
+//       let ul = document.createElement("ul");
+
+//       for(let key in obj) {
+//           let li = document.createElement("li");
+//           li.innerHTML = key;
+//           let childrenUl = createTreeObject(obj[key]);
+//           if(childrenUl) li.append(childrenUl);
+//           ul.append(li);
+//       }
+
+//       return ul;
+//   }
+
+//   let container = document.getElementById("container");
+
+//   createTree(container, data);
+
+// let lis = document.getElementsByTagName('li');
+
+// for(let li of lis) {
+//     let descendantsCount = li.getElementsByTagName('li').length;
+//     if(!descendantsCount) continue;
+
+//     li.firstChild.data += '[' + descendantsCount + ']';
+// }
+
+// alert(count);
+
+// let calendar = document.querySelector('#calendar');
+
+// function createCalendar(elem, year, month) {
+//     let mon = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
+//       let d = new Date(year, mon);
+
+//       let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+
+//       // пробелы для первого ряда
+//       // с понедельника до первого дня месяца
+//       // * * * 1  2  3  4
+//       for (let i = 0; i < getDay(d); i++) {
+//         table += '<td></td>';
+//       }
+
+//       // <td> ячейки календаря с датами
+//       while (d.getMonth() == mon) {
+//         table += '<td>' + d.getDate() + '</td>';
+
+//         if (getDay(d) % 7 == 6) { // вс, последний день - перевод строки
+//           table += '</tr><tr>';
+//         }
+
+//         d.setDate(d.getDate() + 1);
+//       }
+
+//       // добить таблицу пустыми ячейками, если нужно
+//       // 29 30 31 * * * *
+//       if (getDay(d) != 0) {
+//         for (let i = getDay(d); i < 7; i++) {
+//           table += '<td></td>';
+//         }
+//       }
+
+//       // закрыть таблицу
+//       table += '</tr></table>';
+
+//       elem.innerHTML = table;
+// }
+
+// createCalendar(calendar, 2012, 9);
+
+// function getDay(date) {
+//     let day = date.getDay();
+//     if(day === 0) {
+//         day = 7;
+//     }
+//     return day - 1; 
+// }
+
+
+
+// function createClock() {
+//     var today = new Date();
+//     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//     let hours = document.querySelector('.hours');
+//     hours.textContent = today.getHours(); 
+//     let minutes = document.querySelector('.minutes');
+//     minutes.textContent = today.getMinutes();
+//     let seconds = document.querySelector('.seconds');
+//     seconds.textContent = today.getSeconds();
+// }
+
+// setInterval(createClock, 1000);
+// or 
+// function update() {
+//     let clock = document.getElementById('clock');
+//     let date = new Date(); // (*)
+//     let hours = date.getHours();
+//     if (hours < 10) hours = '0' + hours;
+//     clock.children[0].innerHTML = hours;
+  
+//     let minutes = date.getMinutes();
+//     if (minutes < 10) minutes = '0' + minutes;
+//     clock.children[1].innerHTML = minutes;
+  
+//     let seconds = date.getSeconds();
+//     if (seconds < 10) seconds = '0' + seconds;
+//     clock.children[2].innerHTML = seconds;
+//   }
+
+//   let timerId;
+
+// function clockStart() { // запустить часы
+//   timerId = setInterval(update, 1000);
+//   update(); // (*)
+// }
+
+// function clockStop() {
+//   clearInterval(timerId);
+//   timerId = null;
+// }
+
+// let one = document.querySelector('#one');
+// let two = document.querySelector('#two');
+
+// one.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');
+
+let table = document.querySelector('#table');
+
+let sortedRows = Array.from(table.rows)
+.slice(1)
+.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
+table.tBodies[0].append(...sortedRows);
+
+
+

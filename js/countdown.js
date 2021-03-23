7c920ebf812eda8feb3d2f70a059381b5d93d5ed
Vlad <vlchervjakov@gmail.com>
2021-03-23 23:23:54 +0200

    added years and month needs improvement

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,10 @@
 document.addEventListener( 'DOMContentLoaded', () => {
 
-    const date = new Date( 'Jan 1 2022 00:00:00' );
+    const date = new Date( 'Jan 1 2024 00:00:00' );
     
+    const $years = document.querySelector( '.time-count-item-years .time-count-val'  );
+    const $month = document.querySelector( '.time-count-item-month .time-count-val' );
+    const $weeks = document.querySelector( '.time-count-item-weeks .time-count-val' );
     const $days = document.querySelector( '.time-count-item-days .time-count-val' );
     const $hours = document.querySelector( '.time-count-item-hours .time-count-val' );
     const $minutes = document.querySelector( '.time-count-item-minutes .time-count-val' );
@@ -11,6 +14,19 @@ document.addEventListener( 'DOMContentLoaded', () => {
         let now = new Date();
         let timeleft = date - now;
 
+        let years = Math.floor( timeleft / 1000 / 60 / 60 / 24 / 30 / 12 );
+
+       // let month = Math.floor( timeleft / 1000 / 60 / 60 / 24 / 30 );
+
+       month = ( date.getFullYear() - now.getFullYear() ) * 12;
+
+       console.log(month);
+       
+
+        if (month > 12) {
+            month = month % 12;
+        }
+
         let days = Math.floor( timeleft / 1000 / 60 / 60 / 24 ); // get days
 
         let hours = Math.floor( timeleft / 1000 / 60 / 60 ) % 24; // get hours
@@ -19,6 +35,8 @@ document.addEventListener( 'DOMContentLoaded', () => {
 
         let seconds = Math.floor(timeleft / 1000 ) % 60; // get seconds
 
+        $years.textContent = years;
+        $month.textContent = month;
         $days.textContent = days;
         $hours.textContent = hours;
         $minutes.textContent = minutes;

4e284d055c1194d927ca7504c4a1cd4f0df89953
Vlad <vlchervjakov@gmail.com>
2021-07-21 22:06:02 +0300

    scroll-top

---
+++ js/scroll-top.js
@@ -0,0 +1,35 @@
+const offset = 100;
+const scrollUp = document.querySelector( '.scroll-up' );
+const scrollUpSvgPath = document.querySelector( '.scroll-up__svg-path' );
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+// udate dashoffset
+const updateDashoffset = () => {
+    const height = document.documentElement.scrollHeight - window.innerHeight;
+    const dashoffset = pathLength - ( getTop() * pathLength / height )
+    scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+// on scroll
+window.addEventListener( 'scroll', () => {
+    updateDashoffset();
+    if ( getTop() > offset ) {
+        scrollUp.classList.add( 'scroll-up--active' );
+    } else {
+        scrollUp.classList.remove( 'scroll-up--active' );
+    }
+} );
+
+
+// click
+scrollUp.addEventListener( 'click', () => {
+    window.scrollTo( {
+        top: 0,
+        behavior: 'smooth'
+    } );
+} );
